e3f517bf9104821f774d5c133d1ecbe22e550af2
Lucas Santos <lucassantossilvadev@gmail.com>
2026-02-26 16:27:09 -0300

    Add files via upload

---
+++ exercicioComplemento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioComplemento
+{
+    internal class exercicioComplemento
+    {
+        static void Main(string[] args)
+        {
+            
+            int A, B, C, soma, maior;
+
+            Console.WriteLine("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            C = int.Parse(Console.ReadLine());
+
+            soma = A + B + C;
+
+            if (A > B && A > C)
+            {
+                maior = A;
+            }
+            else if (B > C)
+            {
+                maior = B;
+            }
+            else
+            {
+                maior = C;
+            }
+
+            Console.WriteLine("O maior valor é: " + maior);
+
+
+            if (soma % 2 == 0)
+            {
+                Console.WriteLine("A soma dos números é par.");
+            }
+            else
+            {
+                Console.WriteLine("A soma dos números é ímpar.");
+            }
+
+        }
+    }
+}
